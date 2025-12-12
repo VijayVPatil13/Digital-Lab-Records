821dbe503940e4a4ea3b513be55af8a69dd02666
VijayVPatil13 <vijayvpatil13@gmail.com>
2025-12-12 11:42:41 +0530

    Added logo

--- server/server.js
+++ server/server.js
@@ -15,7 +15,12 @@ const { notFound, errorHandler } = require('./middleware/errorMiddleware');
 const app = express();
 
 // Middleware
-app.use(cors());
+app.use(cors({
+  origin: "*", // or specific IPs: [ "http://<client-ip>:3000" ]
+  methods: ["GET", "POST", "PUT", "DELETE"],
+  credentials: true
+}));
+
 app.use(express.json());
 
 // Database Connection
@@ -43,4 +48,6 @@ app.use(errorHandler);
 
 // Port and Server Start
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+app.listen(PORT, '10.20.50.251', () => {
+  console.log(`Server running on http://10.20.50.251:${PORT}`);
+});
